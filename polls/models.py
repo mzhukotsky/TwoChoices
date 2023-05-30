39b2ada538a3e323c4533fa398595cbfeee8f9c9
mzhukotsky <slexmachine@ya.ru>
2023-05-30 21:20:25 +0300

    Added different choices, minor customized admin panel

--- polls/models.py
+++ polls/models.py
@@ -2,10 +2,20 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 class Question(models.Model):
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -17,6 +27,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
