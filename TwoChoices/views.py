54e925d5d2b83d8f8410721367ea77ec5530e8a0
Maxim Zhukotsky <slexmachine@ya.ru>
2023-07-12 13:53:52 +0300

    Delete TwoChoices directory

--- TwoChoices/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-# Create your views here.
-
-def main(request):
-    context = {"main": main}
-    return render(request, "twochoices/main.html", context)
